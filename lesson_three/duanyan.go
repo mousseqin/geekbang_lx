46bef70e2aba34f1e6b05247a59bd0b4235f32fa
mousseqin <qinchao_mousse@icloud.com>
2022-03-20 11:42:48 +0800

    新增断言类型练习

---
+++ lesson_three/duanyan.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// main 练习断言
+func main(){
+	//var v interface{}
+	//
+	//r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	//for i := 0; i < 10; i++{
+	//	v = i
+	//	if (r.Intn(100) % 2) == 0 {
+	//		v = "hello"
+	//	}
+	//	if _, ok := v.(int); ok {
+	//		fmt.Printf("%v \n", v)
+	//	}
+	//}
+
+	var x interface{}
+	x = 10
+	value, ok := x.(int)
+	fmt.Print(value, ",", ok)
+
+}
