88717f1a6db6ffc2374a330a27dd6930b0c55a1b
mousseqin <qinchao_mousse@icloud.com>
2022-03-22 15:40:27 +0800

    up

--- week_one/error_lx.go
+++ week_one/error_lx.go
@@ -1,20 +1,24 @@
 package main
 
 import (
-	"io"
-	"log"
+	"errors"
+	"fmt"
+	xerrors "github.com/pkg/errors"
 )
 
+var errMy = errors.New("my")
+
 func main(){
-	//var e error
-	//errors.Wrap(e, "read failed")
+	err := test2()
+	fmt.Printf("main func :%+v \n",err)
 }
 
-func Write(w io.Writer,buf []byte) error {
-	_ , err := w.Write(buf)
-	if err != nil {
-		log.Println("test success",err)
-		return err
-	}
-	return nil
+func test0 () error {
+	return xerrors.Wrapf(errMy,"test0 failed")
+}
+func test1 () error {
+	return test0()
+}
+func test2 () error {
+	return test1()
 }
