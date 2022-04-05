50ee0a8553f1c25c90b763ab6e650ff6036c7ba8
mousseqin <qinchao_mousse@icloud.com>
2022-04-05 23:28:40 +0800

    新增kratos中context练习

---
+++ kratos/second_lx/context.go
@@ -0,0 +1,99 @@
+package kratos
+
+import (
+	"context"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+type mergeCtx struct {
+	parent1,parent2 context.Context
+
+	done		chan struct{}
+	doneMark	uint32
+	doneOnce	sync.Once
+	doneErr		error
+
+	cancelCh	chan struct{}
+	cancelOnce	sync.Once
+}
+
+func (mc *mergeCtx) finish(err error) error {
+	mc.doneOnce.Do(func(){
+		mc.doneErr = err
+		atomic.StoreUint32(&mc.doneMark,1)
+		close(mc.done)
+	})
+	return mc.doneErr
+}
+
+func (mc *mergeCtx) wait () {
+	var err error
+	select {
+	case <-mc.parent1.Done():
+		err = mc.parent1.Err()
+	case <-mc.parent2.Done():
+		err = mc.parent2.Err()
+	case <-mc.cancelCh:
+		err = context.Canceled
+	}
+	_ = mc.finish(err)
+}
+
+func (mc *mergeCtx) cancel() {
+	mc.doneOnce.Do(func() {
+		close(mc.cancelCh)
+	})
+}
+
+// Done implements context.Context.
+func (mc *mergeCtx) Done() <-chan struct{} {
+	return mc.done
+}
+
+// Err implements context.Context.
+func (mc *mergeCtx) Err() error {
+	if atomic.LoadUint32(&mc.doneMark) != 0 {
+		return mc.doneErr
+	}
+
+	var err error
+	select {
+	case <-mc.parent1.Done() :
+		err =  mc.parent1.Err()
+	case <-mc.parent2.Done():
+		err = mc.parent2.Err()
+	case <-mc.cancelCh:
+		err = context.Canceled
+	default:
+		return nil
+	}
+
+	return mc.finish(err)
+}
+
+// Deadline implements context.Context.
+func (mc *mergeCtx) Deadline()(time.Time,bool){
+	d1 , ok1 := mc.parent1.Deadline()
+	d2 , ok2 := mc.parent2.Deadline()
+
+	switch{
+	case !ok1:
+		return d2 , ok2
+	case !ok2:
+		return d1 , ok1
+	case d1.Before(d2):
+		return d1 , true
+	default:
+		return d2 , true
+	}
+}
+
+// Value implements context.Context.
+func (mc *mergeCtx) Value(key interface{}) interface{} {
+	if v:= mc.parent1.Value(key) ; v != nil {
+		return v
+	}
+	return mc.parent2.Value(key)
+}
