94d265961dea4b1b3fb73e43f8c4f21b222d7e17
mousseqin <qinchao_mousse@icloud.com>
2022-08-28 20:43:28 +0800

    add net lx

---
+++ dmsz/week_three/net/clint_test.go
@@ -0,0 +1,39 @@
+package net
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClient_Send(t *testing.T) {
+	testCases := []struct {
+		req  string
+		resp string
+	}{
+		{
+			req:  "hello",
+			resp: "hello, from response",
+		},
+		{
+			req:  "aaa bbb cc \n",
+			resp: "aaa bbb cc \n, from response",
+		},
+	}
+
+	c := &Client{
+		addr: "localhost:8080",
+	}
+	for _, tc := range testCases {
+		t.Run(tc.req, func(t *testing.T) {
+			resp, err := c.Send(tc.req)
+			assert.Nil(t, err)
+			assert.Equal(t, tc.resp, resp)
+		})
+	}
+}
+
+func TestConnect(t *testing.T) {
+	err := Connect("localhost:8080")
+	assert.Nil(t, err)
+}
